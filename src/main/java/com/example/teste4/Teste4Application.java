f80aede19d498f3acb498b2e1fcc68069b2a2fad
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-01-06 21:47:50 -0300

    Project created

---
+++ src/main/java/com/example/teste4/Teste4Application.java
@@ -0,0 +1,13 @@
+package com.example.teste4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teste4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teste4Application.class, args);
+	}
+
+}
