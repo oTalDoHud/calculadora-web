c2b37feb4cb51edf3865bb9cf87afe7350e1a455
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-01-06 22:47:35 -0300

    User - request findById

--- src/main/java/com/example/teste4/controllers/UserController.java
+++ src/main/java/com/example/teste4/controllers/UserController.java
@@ -1,10 +1,25 @@
 package com.example.teste4.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(params = "/user")
-public class UserController {
+import com.example.teste4.model.User;
+import com.example.teste4.services.UserService;
 
+@RestController
+@RequestMapping(path = "/user")
+public class UserController {
+	
+	@Autowired
+	private UserService service;
+
+	@GetMapping(path = "/id/{id}")
+	public ResponseEntity<User> findById(@PathVariable Integer id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
 }
--- src/main/java/com/example/teste4/services/UserService.java
+++ src/main/java/com/example/teste4/services/UserService.java
@@ -1,8 +1,21 @@
 package com.example.teste4.services;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.teste4.model.User;
+import com.example.teste4.repositories.UserRepository;
 
 @Service
 public class UserService {
 
+	@Autowired
+	private UserRepository userRepo;
+	
+	public User findById(Integer id) {
+		Optional<User> user = userRepo.findById(id);
+		return user.get();
+	}
 }
